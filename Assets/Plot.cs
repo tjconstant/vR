1c472253583bb8e73fcb4c7c1e314f666a2bf8ef
Tom Constant <t.j.constant@gmail.com>
2018-02-06 15:54:41 +0000

    Adding new Methods

--- Assets/Plot.cs
+++ Assets/Plot.cs
@@ -12,32 +12,44 @@ public class Plot : MonoBehaviour {
 	private float[] zCoords = {0,1,2,3,4};
 
 	void Start () {
-
-		// Plot Points using Spheres
-		for (int i = 0; i < xCoords.Length; i++) {
-			var myPoint = Instantiate(point, canvas.transform.position + new Vector3(xCoords[i], yCoords[i], zCoords[i]), Quaternion.identity);
-			myPoint.transform.parent = canvas.transform;
-		}
-
-		// Plot Line unsing LineRenderer
-		LineRenderer myLine = canvas.GetComponent<LineRenderer>();
-		var coordArray = MakeCoordArray (xCoords, yCoords, zCoords);
-		myLine.positionCount = coordArray.Length;
-		myLine.SetPositions (coordArray);
-		myLine.enabled = true;
-
+		GeomPoint ();
+		GeomLine (); 
 	}
 
-	// Make a Coordinate Arrayc of type Vaector3
+	// Make a Coordinate Array of type Vector3
 	Vector3[] MakeCoordArray (float[] x, float[] y, float[] z){
 		
-		Vector3[] coords = new Vector3[5];
+		Vector3[] coords = new Vector3[xCoords.Length];
 		for (int i = 0; i < x.Length; i++) {
 			coords [i] = new Vector3 (x [i], y [i], z [i]);
 		}
 
 		return coords;
 	}
+
+	//public class Geom {
+
+	void GeomPoint () {
+		// Plot Points using Spheres
+		for (int i = 0; i < xCoords.Length; i++) {
+			var myPoint = Instantiate(point, canvas.transform.position + new Vector3(xCoords[i], yCoords[i], zCoords[i]), Quaternion.identity);
+			myPoint.transform.parent = canvas.transform;
+		}
+	}
+
+	void GeomLine () {
+		// Plot Line unsing LineRenderer
+
+		//LineRenderer myLine = canvas.GetComponent<LineRenderer>();
+		LineRenderer myLine = canvas.AddComponent<LineRenderer>();
+		myLine.useWorldSpace = false;
+		var coordArray = MakeCoordArray (xCoords, yCoords, zCoords);
+		myLine.positionCount = coordArray.Length;
+		myLine.SetPositions (coordArray);
+		myLine.enabled = true;
+	}
+	
+	//}
 
 }
 
