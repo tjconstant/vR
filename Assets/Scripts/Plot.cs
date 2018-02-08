d90c3c8d9b8cf62fbf3108c48f1235651f701658
Tom Constant <t.j.constant@gmail.com>
2018-02-08 15:24:38 +0000

    Read CSV tweaked, still not perfect

--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -8,27 +8,25 @@ public class Plot : MonoBehaviour {
 	public GameObject canvas;
 	public GameObject point;
 
-	private float[] xCoords = {0,1,2,3,4,8};
-	private float[] yCoords = {0,1,2,3,4,5};
-	private float[] zCoords = {0,1,2,2,4,2};
+	//private float[] xCoords = {0,1,2,3,4,8};
+	//private float[] yCoords = {0,1,2,3,4,5};
+	//private float[] zCoords = {0,1,2,2,4,2};
 
 	private int canvasSize = 10;
 
 	void Start () {
 
-
-		Vector3[] tom = read_csv ("C:\\Users\\Tom\\Documents\\text.csv");
-		Debug.Log (tom);
+		Vector3[] myData = read_csv ("C:\\Users\\Tom\\Documents\\text.csv");
 
 		// Coordinate Normalization - Doesn't Work Yet for Negative Numbers
-		for (int i = 0; i < xCoords.Length; i++) {
-			xCoords [i] = (canvasSize * xCoords [i] / Mathf.Max (xCoords)) - canvasSize/2;
-			yCoords [i] = canvasSize * yCoords [i] / (Mathf.Max (yCoords) + 0.1F); // +0.1 to avoid problems if max is zero (need better solution)
-			zCoords [i] = (canvasSize * zCoords [i] / Mathf.Max (zCoords)) - canvasSize/2;
-		}
+		//for (int i = 0; i < xCoords.Length; i++) {
+		//	xCoords [i] = (canvasSize * xCoords [i] / Mathf.Max (xCoords)) - canvasSize/2;
+		//	yCoords [i] = canvasSize * yCoords [i] / (Mathf.Max (yCoords) + 0.1F); // +0.1 to avoid problems if max is zero (need better solution)
+		//	zCoords [i] = (canvasSize * zCoords [i] / Mathf.Max (zCoords)) - canvasSize/2;
+		//}
 
-		GeomPoint (tom);
-		GeomLine (tom); 
+		GeomPoint (myData);
+		GeomLine (myData); 
 	}
 
 	// Make a Coordinate Array of type Vector3
@@ -48,11 +46,13 @@ public class Plot : MonoBehaviour {
 		string str = System.IO.File.ReadAllText (filename);
 		string[] values = str.Split('\n');
 
-		float[] xCoords = new float[100];
-		float[] yCoords = new float[100]; 
-		float[] zCoords = new float[100]; 
 
-		for(int i = 1; i<100; i++){
+		// Including an empty 0,0 point that needs to be removed
+		float[] xCoords = new float[values.Length-1]; //-1 to remove
+		float[] yCoords = new float[values.Length-1]; 
+		float[] zCoords = new float[values.Length-1]; 
+
+		for(int i = 1; i<values.Length-1; i++){
 			xCoords[i] = float.Parse(values [i].Split (',')[0]);
 			yCoords[i] = float.Parse(values [i].Split (',')[2]);
 			zCoords[i] = float.Parse(values [i].Split (',')[1]);
@@ -66,6 +66,7 @@ public class Plot : MonoBehaviour {
 		// Plot Points using Spheres
 		for (int i = 0; i < coordArray.Length; i++) {
 			var myPoint = Instantiate(point, canvas.transform.position + coordArray[i], Quaternion.identity);
+			//myPoint.transform.localScale = new Vector3(0.1F,0.1F,0.1F);
 			myPoint.transform.parent = canvas.transform;
 		}
 	}
@@ -76,7 +77,6 @@ public class Plot : MonoBehaviour {
 		LineRenderer myLine = canvas.GetComponent<LineRenderer>();
 		//LineRenderer myLine = canvas.AddComponent<LineRenderer>();
 		myLine.useWorldSpace = false;
-		//var coordArray = MakeCoordArray (xCoords, yCoords, zCoords);
 		myLine.positionCount = coordArray.Length;
 		myLine.SetPositions (coordArray);
 		myLine.widthMultiplier = 0.3F;
